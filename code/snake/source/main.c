c1509e187b30d5709bcc841cbce462e73bdcce3e
mazcunyanbla <mazcunyanbla@esat-miralles.com>
2024-03-26 11:20:46 +0100

    add snake

--- code/snake/source/main.c
+++ code/snake/source/main.c
@@ -94,7 +94,7 @@ int UpdateSnake (int keypad, Snake* snake, unsigned short* screen)
 
   int speed_x = snake->speed_x;
   int speed_y = snake->speed_y;  
-  if (keypad != 0) {
+  /*if (keypad != 0) {
     speed_x = speed_y = 0;
     if (keypad & 1)
       speed_x = 1;
@@ -104,10 +104,10 @@ int UpdateSnake (int keypad, Snake* snake, unsigned short* screen)
       speed_y = -1;
     if (keypad & 8)
       speed_y = 1;
-
+*/
     snake->speed_x = speed_x;
     snake->speed_y = speed_y;
-  }
+  //}
 
   // Head new pos.
   new_x += speed_x;
--- code/snake/source/snake_funcs.s
+++ code/snake/source/snake_funcs.s
@@ -2,16 +2,18 @@
 
 @ typedef struct
 @ {
-@   int x, y;
-@ } Slab;
+@   int x; 0
+@   int y; 4
+@ } Slab;  8
 
 @ typedef struct
 @ {
-@   Slab* slabs;
-@   unsigned short len;
-@   unsigned short head_slab;
-@   int   speed_x, speed_y;
-@ } Snake;
+@   Slab* slabs;    0
+@   unsigned short len; 4
+@   unsigned short head_slab; 6
+@   int   speed_x; 8
+@   int   speed_y; 12
+@ } Snake; 16
 
 @ static Slab snake_slabs [SNAKE_LEN];
 
@@ -26,15 +28,42 @@
 @    .global UpdateSnake
 UpdateSnake:
     stmdb   sp!,{r4,r5,r6,r7,r8,r9,r10,r11,r12,r14}
-    mov r3,#0     @ int crash = 0
-    ldr r4,[r1]   @ r4 = snake->slabs 
-    str r4,[r4]   @ slabs = r4
+    mov  r3,#0               @ int crash = 0
+    ldr  r4,[r1]             @ Slab* slabs = snake->slabs 
+    ldrh r5,[r1,#6]          @ int head_slab = snake->head_slab
+    add  r6,r5,#1            @ int tail = snake->head_slab + 1;
 
+    ldrh r7,[r1,#4]          @ r7 = snale->len
+    cmp  r6,r7               @ if(tail >= snake_len)                                                                                                                          
+    movge  r6,#0             @ tail = 0   
+
+    add  r10,r4,r6,lsl #3    @ r10 = slabs[tail]                                                                                        
+    ldr  r7,[r10]            @ slabs[tail].x
+    ldr  r8,[r10,#4]              @  slabs[tail].y                                                           
+    mov  r9, #240                  @  r9 = 240                  
+    mul  r8,r9,r8                 @  slabs[tail].y * 240
+    add  r8,r8,r7                 @  slabs[tail].x + slabs[tail].y * 240
+    add  r8,r8,r8                 @  r8 * 2    
+    mov  r7,#0                    @  r7 = 0
+    strh r7,[r2,r8]               @  screen[slabs[tail].x + slabs[tail].y * 240] = 0; // BLACK    
+                                  
+    ldrh r7,[r1,#6]               @ r7 = snake->head_slab
+    add  r7,r4,r7,lsl #3          @ r7 = slabs[snake->head_slab]
+    ldr  r8,[r7]                  @  int new_x = slabs[snake->head_slab].x;
+    ldr  r9,[r7,#4]               @  int new_y = slabs[snake->head_slab].y;
+
+    add  r5,r5,#1                 @  head_slab++;
+
+    ldrh r7,[r1,#4]           @ r7 = snake->len
+    cmp  r5,r7                @ if(head_slab >= snake->len)
+    movge r5,#0               @ head_slab = 0
+    strh   r5,[r1,#6]          @ snake->head_slab = head_slab
+
+@  int speed_x = snake->speed_x;
+@  int speed_y = snake->speed_y;
+    mov   r0,r3
     ldmia   sp!,{r4,r5,r6,r7,r8,r9,r10,r11,r12,r14}
     bx      lr
-
-
-
 
 
 
@@ -51,5 +80,35 @@ UpdateSnake:
 
 @  screen[slabs[tail].x + slabs[tail].y * 240] = 0; // BLACK 
 
-@   return crash;
+@  int new_x = slabs[snake->head_slab].x;
+@  int new_y = slabs[snake->head_slab].y;
+
+@  head_slab++;
+@  if (head_slab >= snake->len)
+@    head_slab = 0;
+@  snake->head_slab = head_slab;
+
+@@  int speed_x = snake->speed_x;
+@@  int speed_y = snake->speed_y;
+
+@@  snake->speed_x = speed_x;
+@@  snake->speed_y = speed_y;
+
+@@  new_x += speed_x;
+@@  new_y += speed_y;
+
+@@  unsigned short* head_pix = screen + (new_x + new_y * 240);
+@@  if (*head_pix == 0) {
+@@    *head_pix = 0xffff; // Paint head, white
+
+@@    slabs[head_slab].x = new_x;
+@@    slabs[head_slab].y = new_y;
+
+@@  } 
+@@  else
+@@    crash = 1;
+
+
+@  keypad = crash;
+@  return keypad;
 @ }
