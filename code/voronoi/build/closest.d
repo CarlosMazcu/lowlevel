5c90d3c6d47ce80c0a4f016e2a6211536d6bf9fe
Carlos Mazcu <118178013+CarlosMazcu@users.noreply.github.com>
2024-04-10 09:27:52 +0200

    df

--- code/voronoi/build/closest.d
+++
@@ -1,4 +0,0 @@
-closest.o: /home/esat/Desktop/lowlevel/code/voronoi/source/closest.c \
- /home/esat/Desktop/lowlevel/code/voronoi/source/tpoint.h
-
-/home/esat/Desktop/lowlevel/code/voronoi/source/tpoint.h:
--- code/voronoi/build/extra_ref_test.d
+++
@@ -1,5 +0,0 @@
-extra_ref_test.o: \
- /home/esat/Desktop/lowlevel/code/voronoi/source/extra_ref_test.c \
- /home/esat/Desktop/lowlevel/code/voronoi/source/tpoint.h
-
-/home/esat/Desktop/lowlevel/code/voronoi/source/tpoint.h:
