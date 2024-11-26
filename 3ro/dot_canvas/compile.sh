13c1dd5a076ddea96ad3b47d10ca63e6295a7fc1
mazcunyanbla <mazcunyanbla@esat-libertad.com>
2024-11-26 18:01:24 +0100

    landscape

--- 3ro/dot_canvas/compile.sh
+++ 3ro/dot_canvas/compile.sh
@@ -1,6 +1,6 @@
-
+# para optimizar y medir O2 , para debugear O0
 # Para compilar en linux x86/x64
-gcc -c -g -Wall -O2 -o obj/dot_canvas.o dot_canvas.c
+gcc -c -g -Wall -O2 -o obj/landscape.o landscape.c
 # Para compilar ARM
 #gcc -c -g -Wall -mfpu=vfp -O2 -o obj/dot_canvas.o dot_canvas.c
 
@@ -8,4 +8,4 @@ gcc -c -g -Wall -O2 -o obj/chrono.o chrono.c
 
 cd obj
 
-gcc -o ../dot_canvas.elf dot_canvas.o chrono.o  -lm -lSDL 
+gcc -o ../landscape.elf landscape.o chrono.o  -lm -lSDL 
